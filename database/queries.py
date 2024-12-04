773e41f5a2c2b7d5ad9b9161a711827e831feec0
archery <omrigamer100@gmail.com>
2024-12-04 22:07:36 +0200

    queries script ready almost

--- database/queries.py
+++ database/queries.py
@@ -20,11 +20,31 @@ def insert_root():
         postgres = get_database_connection()
         cursor = postgres.cursor()
 
+        root_nodes = [
+            {
+                "title": "ubuntu",
+                "description": "ubuntu machine",
+                "status": "up",
+            },
+            {
+                "title": "kubernetes",
+                "description": "kubernetes data",
+                "status": "expired",
+            }    
+        ]
+
+        for root_node in root_nodes:
+            title = root_node["title"]
+            description = root_node["description"]
+            status = root_node["status"]
+
+            print(title, description, status)
 
 
-        # cursor.execute("insert into nodes (title, description) values (%s, %s)", ())
-
+        cursor.execute("insert into nodes (title, description, status) values (%s, %s, %s)", (title, description, status))
+        postgres.commit()
         
-    
     except Exception as e:
         print(e)
+
+insert_root()
