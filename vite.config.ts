936947bc136f011d88a512ec315bc0e682dd6ca1
omri <omrigamer100@gmail.com>
2024-12-20 13:56:54 +0200

    changed vite  config to open network

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Expose to local network
+    port: 3000, // Optional: set a specific port
+    open: true, // Optional: open in browser automatically
+  },
 })
