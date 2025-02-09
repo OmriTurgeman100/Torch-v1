f393673e2a05940f0c1819fc82801d1f48433603
omri <omrigamer100@gmail.com>
2025-02-09 16:32:44 +0200

    test

---
+++ tests/script_5.py
@@ -0,0 +1,39 @@
+import requests
+import time
+import logging
+import json
+from datetime import datetime
+import random
+
+def black_box_script():
+    try:
+
+        api = "http://localhost/api/v1/post/reports"
+
+        headers = {
+            "Content-Type": "application/json"
+        }
+
+        body = {
+            "report_id": 'sample report id dev',
+            "title": 'sample report title dev',
+            "description": 'sample report description dev',
+            # "value": random.choice([50, 100]),
+            "value": 60
+        }
+
+        response = requests.post(api, data=json.dumps(body), headers=headers)
+ 
+
+        print(body["report_id"], body["title"], body["description"], body["value"])
+        print(response.status_code)
+        data = response.json()
+
+        print(data)
+
+    except Exception as e:
+        print(e)
+
+while True:
+    black_box_script()
+    # time.sleep(1)
